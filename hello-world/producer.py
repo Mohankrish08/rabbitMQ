e7af54624ebff430515a2a9bdab4ea0ef40c17ad
[Mohan krishnan O] <81806134+Mohankrish08@users.noreply.github.com>
2026-07-15 18:53:21 +0530

    first producer and consumer in rabbitmq

---
+++ hello-world/producer.py
@@ -0,0 +1,16 @@
+# Importing libraries
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
+channel.basic_publish(
+    exchange='',
+    routing_key='hello',
+    body='Hello world!'
+)
+print(" [X] Sent 'Hello World!'")
+connection.close()
+
